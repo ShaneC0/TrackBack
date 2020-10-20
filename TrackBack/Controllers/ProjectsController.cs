99193725725618b967a72a3961463dbf5e97c92a
Shane Connors <connors.shane@hotmail.com>
2020-10-20 19:05:38 -0400

    initial commit

---
+++ TrackBack/Controllers/ProjectsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrackBack.Data;
+using TrackBack.Models;
+
+namespace TrackBack.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProjectsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var projectItems = _context.Projects;
+            return View(projectItems);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var projectToEdit = _context.Projects.Find(id);
+            if(projectToEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(projectToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Project project)
+        {
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var projectToDelete = _context.Projects.Find(id);
+            if (projectToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(projectToDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TrackBack/Migrations/20201020220013_addProjectTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrackBack.Migrations
+{
+    public partial class addProjectTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
